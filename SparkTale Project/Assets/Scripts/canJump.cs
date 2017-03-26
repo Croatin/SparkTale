abf79d9ff454f116b26e590a042d0c4d4876883f
Croatin <adriana.gabrielle.rivera@gmail.com>
2017-03-26 17:54:07 -0400

    initial commit for game files. Basic controls added, as well as basic physics implemented.

---
+++ SparkTale Project/Assets/Scripts/canJump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canJump : MonoBehaviour {
+
+    void OnCollisionStay(Collision col)
+    {
+        GameObject.FindObjectOfType<playerController>().canJump = true;
+    }
+}
---
+++ SparkTale Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float movementSpeed = 25, jumpHeight = 5;
+
+    public bool canJump;
+    private GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        float movementY = Input.GetAxis("Vertical") * movementSpeed;
+        float movementX = Input.GetAxis("Horizontal") * movementSpeed;
+
+        Player.transform.Translate(movementX, 0, movementY);
+
+        if(Input.GetButtonDown("Jump") && canJump)
+        {
+            Player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight);
+        }
+    }
+}
